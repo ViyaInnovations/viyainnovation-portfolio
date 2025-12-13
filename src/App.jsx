3c748d2a63759ddb280ecc675a0bb9e8c73553a6
dilaksh06 <kamalathasandilakshan@gmail.com>
2025-12-13 11:44:06 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import NavBar from "./components/NavBar";
+
 import Home from "./components/Home";
 import About from "./components/About";
+import Contac from "./components/Contact";
 function App() {
 
   return (
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contac />} />
+
       </Routes>
     </BrowserRouter>
   );
