3ffeccc66f8086c7171acfbaccb75ef08ba0f184
Hussain <hussainmhn75@gmail.com>
2025-12-13 12:16:35 +0530

    cn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 function App() {
